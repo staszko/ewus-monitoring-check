ac0c0e1a1b376df73d58f6417d972bc3da9f7e0f
staszko <30053337+staszko@users.noreply.github.com>
2021-03-21 17:26:41 +0100

    Loginparams do Auth service

--- main.py
+++ main.py
@@ -3,4 +3,16 @@ import zeep
 wsdl = 'https://ewus.nfz.gov.pl/ws-broker-server-ewus-auth-test/services/Auth?wsdl'
 client = zeep.Client(wsdl=wsdl)
 
-print(client._get_service("Auth"))
+#print(client._get_service("Auth"))
+factory = client.type_factory('ns3')
+domianParamValue = factory.paramValue(stringValue='15')
+domainLoginParam = factory.loginParam(name='domain', value=domianParamValue)
+
+
+loginParamValue = factory.paramValue(stringValue='LEKARZ1')
+loginLoginParam = factory.loginParam(name='login', value=loginParamValue)
+
+loginParams = factory.loginParams(item=[domainLoginParam, loginLoginParam])
+
+print(loginParams)
+
