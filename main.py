616823ff400b99a83c00ae9fc8b9a37ea092db6f
staszko <30053337+staszko@users.noreply.github.com>
2021-03-22 16:24:53 +0100

    reactoring

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import zeep
 
 wsdl = 'https://ewus.nfz.gov.pl/ws-broker-server-ewus-auth-test/services/Auth?wsdl'
+ewusLogin='LEKARZ1'
+ewusPassword='qwerty!@#'
+ewusDomain = '15'
+
+
 client = zeep.Client(wsdl=wsdl)
 
 #print(client._get_service("Auth"))
 factory = client.type_factory('ns3')
-domianParamValue = factory.paramValue(stringValue='15')
+domianParamValue = factory.paramValue(stringValue=ewusDomain)
 domainLoginParam = factory.loginParam(name='domain', value=domianParamValue)
 
 
-loginParamValue = factory.paramValue(stringValue='LEKARZ1')
+loginParamValue = factory.paramValue(stringValue=ewusLogin)
 loginLoginParam = factory.loginParam(name='login', value=loginParamValue)
 
 loginParams = factory.loginParams(item=[domainLoginParam, loginLoginParam])
 
-result = client.service.login(credentials=loginParams, password='qwerty!@#')
+result = client.service.login(credentials=loginParams, password=ewusPassword)
 
 print(result['header'])
 sessionId = result['header']['session']['id']
